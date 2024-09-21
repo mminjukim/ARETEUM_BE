3c322d3f0271313782aaa91e85d5070db51626e0
yunnij <cindy21cx@gmail.com>
2024-09-21 20:24:29 +0900

    fix cors setting

--- areteumProject/areteumProject/settings.py
+++ areteumProject/areteumProject/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', 
 	'django.middleware.common.CommonMiddleware', 
-    
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,6 +58,16 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True 
+
+CORS_ALLOW_METHODS = [
+	'DELETE',
+	'GET',
+	'OPTIONS',
+	'PATCH',
+	'POST',
+	'PUT',
+]
 
 REST_FRAMEWORK = {
     # CamelCaseJSON 관련 설정
