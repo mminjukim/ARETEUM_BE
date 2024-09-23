9bf708c46e86b15d8fe1a0c596d46261363726d8
Yerim <yerim0307@gmail.com>
2024-09-23 14:21:52 +0900

    Apply get_qureyset

--- areteumProject/booths/views.py
+++ areteumProject/booths/views.py
@@ -110,5 +110,7 @@ class SearchBoothView(generics.ListAPIView):
     
 # 솜톡
 class SomTalkViewSet(ModelViewSet):
-    queryset = SomTalk.objects.all().order_by('-time')[:20][::-1] #오름차순
     serializer_class = SomTalkSerializer
+    
+    def get_queryset(self):
+        return SomTalk.objects.all().order_by('-time')[:20][::-1] #오름차순
